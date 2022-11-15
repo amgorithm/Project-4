2f06d4dbc1359dc4f0b5d995a703a7ba37a31099
Amal <amal.mukhtar@outlook.com>
2022-11-15 15:20:29 +0000

    Tested models and updated models

--- server/foods/models.py
+++ server/foods/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 # Create your models here.
 
 class Category(models.Model):
-  name = models.CharField(max_length=20)
+  name = models.CharField(max_length=50)
 
   def __str__(self):
     return f"{self.name}"
@@ -12,9 +12,9 @@ class Category(models.Model):
 class Food(models.Model):
   name = models.CharField(max_length=50)
   category = models.ForeignKey(Category, related_name='category', on_delete=models.SET_NULL, null=True)
-  quantity = models.IntegerField(default=1)
+  quantity = models.IntegerField(default=1, blank=True, null=True)
   expiry_date = models.DateField()
-  wasted = models.BooleanField()
+  wasted = models.BooleanField(blank=True, null=True)
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
