0c1aff9906d510a3d271e22ef983cd13b270cf03
Amal <amal.mukhtar@outlook.com>
2022-11-27 22:21:51 +0000

    About section css added

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h2>About</h2>
+      </div>
+
+      <div>
+        <img
+          src={require("../../images/food-track.jpg")}
+          alt="grocery bag"
+          className="about-img"
+        />
+      </div>
+      <div className="about-info">
+        <p>
+          Waste Not helps you prevent food waste and money loss by tracking the
+          freshness of your produce and getting analytics on what you have
+          consumed or wasted.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
