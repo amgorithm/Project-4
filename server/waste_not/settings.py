c109183a621c258d19e82ac84141faf8ed099420
Amal <amal.mukhtar@outlook.com>
2022-12-08 19:55:31 +0000

    dotenv installed

--- server/waste_not/settings.py
+++ server/waste_not/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 import environ
 from environ import Env
 import os
